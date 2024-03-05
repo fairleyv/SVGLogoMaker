abf8a87dfb45fbd43c001c3bb9c3183ca44d33c5
fairleyv <fairleyvincent@gmail.com>
2024-03-04 19:27:20 -0800

    initial setup and comments

---
+++ index.js
@@ -0,0 +1,15 @@
+// TODO import inquirer
+const inquirer = require('inquirer');
+// TODO import shapes
+const circle = require('./lib/circle');
+const square = require('./lib/square');
+const triangle = require('./lib/triangle');
+
+// TODO Create questions for inquirer
+const questions = [{}]
+
+// TODO Use Inquirer to get data for making SVG Logo
+
+// TODO create a function that makes the SVG Data using inquirer
+
+// TODO Write SVG data to a new file called logo.svg 
